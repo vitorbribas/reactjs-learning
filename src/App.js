dbc423b3de6b3f6200fbe1c38d2d5e829c21d62c
Vítor <bsbribas66@gmail.com>
2020-03-22 19:43:56 -0300

    create my first component and your styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
+import './styles.css'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hey, Vítor</h1>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <Header />
+  </div>
+);
 
 export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+
+const Header = () => (      // syntax for components without State
+  <header id="main-header">Hello</header>
+);
+
+//class Header extends Component {
+//  render() {
+//    return <h1>Hello</h1>
+//  }
+//}
+
+export default Header;
