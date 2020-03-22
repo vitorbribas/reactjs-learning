c905e04ef5c4fa73471247cac72316ecb5c01452
Vítor <bsbribas66@gmail.com>
2020-03-22 20:04:10 -0300

    add Main component

---
+++ src/pages/main/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import api from '../../services/api';
+
+export default class Main extends Component {
+  render() {
+    return <h1>Heloooooooooo</h1>
+  }
+};
+
