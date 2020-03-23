fd0e3a9c758e4f3dd088edb2ad0b54b255706dc9
Vítor <bsbribas66@gmail.com>
2020-03-23 20:34:51 -0300

    add pagination to products index page

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -4,21 +4,45 @@ import './styles.css';
 
 export default class Main extends Component {
   state = {
-    products: []
+    products: [],
+    productInfo: {},
+    page: 1,
   };
 
   componentDidMount() {
     this.loadProducts();
   }
 
-  loadProducts = async () => {
-    const response = await api.get('/products');
+  loadProducts = async (page = 1) => {
+    const response = await api.get(`/products?page=${page}`);
 
-    this.setState({ products: response.data.docs });
+    const { docs, ...productInfo } = response.data;
+
+    this.setState({ products: docs, productInfo, page });
+  };
+
+  prevPage = () => {
+    const { page } = this.state;
+
+    if (page === 1) return;
+
+    const pageNumber = page - 1;
+
+    this.loadProducts(pageNumber);
+  };
+
+  nextPage = () => {
+    const { page, productInfo } = this.state;
+
+    if ( page === productInfo.pages ) return; // producsInfo.pages stores the higher page number
+
+    const pageNumber = page + 1;
+
+    this.loadProducts(pageNumber);
   };
 
   render() {    // método render fica "escutando" as variáveis do estado e renderiza novamente se ouver alteração
-    const { products } = this.state;
+    const { products, page, productInfo } = this.state;
 
     return (
       <div className="products-list">
@@ -29,6 +53,10 @@ export default class Main extends Component {
             <a href="#">Detalhes</a>
           </article>
         ))}
+        <div className="actions">
+          <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+          <button disabled={page === productInfo.pages} onClick={this.nextPage}>Próxima</button>
+        </div>
       </div>
     );
   }
